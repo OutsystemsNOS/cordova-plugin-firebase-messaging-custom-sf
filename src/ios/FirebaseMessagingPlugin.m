aeabf21f15e3e4dc41d51889ad47b5306caf0c75
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-05-26 12:30:29 +0100

    Update FirebaseMessagingPlugin.m

--- src/ios/FirebaseMessagingPlugin.m
+++ src/ios/FirebaseMessagingPlugin.m
@@ -1,7 +1,7 @@
 #import "FirebaseMessagingPlugin.h"
 #import <Cordova/CDV.h>
 #import "AppDelegate.h"
-#import "UIKit/UIKit.h"
+#import <UserNotifications/UserNotifications.h>
 
 //@import Firebase;
 @import FirebaseMessaging;
@@ -20,20 +20,14 @@
 //mlrosa - Added a new method to check if notifications are ative or not
 - (void)hasPermission:(CDVInvokedUrlCommand *)command {
     @try {
-        BOOL enabled = NO;
-        UIApplication *application = [UIApplication sharedApplication];
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+            BOOL granted = settings.authorizationStatus == UNAuthorizationStatusAuthorized;
     
-        if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
-            enabled = application.currentUserNotificationSettings.types != UIUserNotificationTypeNone;
-        } else {
-            enabled = application.enabledRemoteNotificationTypes != UIRemoteNotificationTypeNone;
-        }
-    
-        NSString *isEnabledString = enabled ? @"true" : @"false"; // Convert BOOL to lowercase string
-    
-        CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:isEnabledString];
-        [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];    
-        }
+            NSString *isEnabledString = granted ? @"true" : @"false";
+            CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:isEnabledString];
+            [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+    }];
     @catch (NSException *exception) {
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
                                                         messageAsString:[NSString stringWithFormat:@"Erro ao verificar permissões: %@", exception.reason]];
